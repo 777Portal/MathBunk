942b0a2d638bbefa030b70361b22790d4eb99564
exon <throwawayact69xd@gmail.com>
2025-01-15 14:57:57 -0500

    Google auth

--- src/auth/login.js
+++ src/auth/login.js
@@ -3,15 +3,15 @@ const db = require('../db/db.js');
 
 async function attemptLogin(username, email){
     let createUser = await user.createUser({username, email});
-    if (createUser.succeded) return {succeded: true, code:"succesful_account_creation", reason:"Created account."};
+    if (createUser.succeded) return {succeded: true, code:"succesful_account_creation", reason:"Created account.", };
     
-    let code = createUser.code
-    if (code !== "email_already_exists") return user.createUser; // else don't really matter, client can handle.
-
+    let code = createUser.code;
+    if (code !== "email_already_exists") return createUser; // else don't really matter, client can handle.
+    
     // login users
     const userInfo = await db.findOne("mathbunk", "users", {"email": email});
     console.log('user info: '+userInfo.email)
-    return {succeded: true, code:"successful_login", reason:"Loged in."};
+    return {succeded: true, code:"successful_login", reason:"Loged in.", userInfo};
 }
 
 module.exports = {
--- src/routes/auth/google/google.js
+++ src/routes/auth/google/google.js
@@ -1,18 +1,64 @@
 const express = require('express');
 const router = express.Router();
 
-const { 
-  web: GOOGLE_CONFIG, 
+const db = require('../../../db/db.js');
+const login = require('../../../auth/login.js');
+
+const passport = require("passport");
+
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+const {
+  web, 
 } = require('../../../../config/googleConfig.json');  
+const { attemptLogin } = require('../../../auth/login.js');
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: web.client_id,
+    clientSecret: web.client_secret,
+    callbackURL: "http://localhost:3000/auth/google/callback",
+    passReqToCallback: true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+));
 
 // give the redirct link
-router.get('/google', (req, res) => {
-  res.status(500).send('Google Authentication Failed');
+router.get('/google',
+  passport.authenticate('google', {
+    scope:
+      ['email', 'profile']
+  }
+));
+
+router.get('/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/',
+  }),
+  function (req, res) {
+    res.redirect('/auth/google/success')
+    // console.log(req.user)
+  }
+);
+router.get('/google/success', async (req, res) => {
+  if (!req.user)
+    return res.redirect("/auth/google");
+
+  req.session.authenticated = true;
+  login.attemptLogin("exonautoo", req.user.email) // its funny that i added the succeded boolean and the reasons just to end up not using them.
+  let data = await db.findOne("mathbunk", "users", {"email": req.user.email});
+  req.session.info = data
+  return res.redirect("/");
 });
 
-// get the code n do stuff with it.
-router.get('/google/callback', async (req, res) => {
-  res.status(500).send('Google Authentication Failed');
-});
 
 module.exports = router;
